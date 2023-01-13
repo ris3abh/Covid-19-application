d5d6c3bb05b7d1f234f73c0e75508f6fe47db9d1
Rishabh Sharma <59526954+ris3abh@users.noreply.github.com>
2023-01-13 17:44:32 -0500

    chand par hai apun

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
+import gaitonde from './assets/gaitonde.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={gaitonde} className="App-logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.imdb.com/title/tt6077448/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <br/>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          Chaand Pe hai Apun lavdia,  Idhar components call karr Bhosdike
         </a>
       </header>
     </div>
